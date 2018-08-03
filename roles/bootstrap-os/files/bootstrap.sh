506b35793a9fee0b345e76e88587fa13420c2c48
Neil Mayhew <neil@fpcomplete.com>
2018-08-03 17:05:03 -0600

    Use portable pypy
    This avoids a problem with CoreOS 1800 onwards which moved from
    ncurses 5 to 6
    
    * https://github.com/kubernetes-incubator/kubespray/issues/3026
    * https://github.com/defunctzombie/ansible-coreos-bootstrap/issues/44

--- roles/bootstrap-os/files/bootstrap.sh
+++ roles/bootstrap-os/files/bootstrap.sh
@@ -11,14 +11,10 @@ if [[ -e $BINDIR/.bootstrapped ]]; then
   exit 0
 fi
 
-PYPY_VERSION=5.1.0
+PYPY_VERSION=6.0.0
 
-wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
-mv -n pypy-$PYPY_VERSION-linux64 pypy
-
-## library fixup
-mkdir -p pypy/lib
-ln -snf /lib64/libncurses.so.5.9 $BINDIR/pypy/lib/libtinfo.so.5
+wget -O - https://bitbucket.org/squeaky/portable-pypy/downloads/pypy-${PYPY_VERSION}-linux_x86_64-portable.tar.bz2 |tar -xjf -
+mv -n pypy-${PYPY_VERSION}-linux_x86_64-portable pypy
 
 cat > $BINDIR/python <<EOF
 #!/bin/bash
